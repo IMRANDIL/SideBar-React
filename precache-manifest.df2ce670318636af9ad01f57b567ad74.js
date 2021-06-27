011ed185ea676419252645265dba9a73fcf5cbe8
IMRANDIL <aliimranadil2@gmail.com>
2021-06-27 16:04:56 +0530

    Updates

---
+++ precache-manifest.df2ce670318636af9ad01f57b567ad74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbc5405507205d5c8205ee94bc6ea98",
+    "url": "/SideBar-React/index.html"
+  },
+  {
+    "revision": "05446e450e65f602d800",
+    "url": "/SideBar-React/static/css/main.e0f79945.chunk.css"
+  },
+  {
+    "revision": "00dabe24ad094e2a245a",
+    "url": "/SideBar-React/static/js/2.43f1e553.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SideBar-React/static/js/2.43f1e553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05446e450e65f602d800",
+    "url": "/SideBar-React/static/js/main.813a51cc.chunk.js"
+  },
+  {
+    "revision": "c60f91862b34724c469a",
+    "url": "/SideBar-React/static/js/runtime-main.ae673998.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/SideBar-React/static/media/logo.2bb7da65.svg"
+  }
+]);
